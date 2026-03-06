dc88acf6565df7832ac3688ea75f7e458fc6e382
samaelMTA <miguelcolunche0@gmail.com>
2026-03-07 00:50:27 +0100

    feat: generated base modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ShipmentsModule } from './shipments/shipments.module';
+import { TrackingModule } from './tracking/tracking.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ShipmentsModule, TrackingModule],
   controllers: [AppController],
   providers: [AppService],
 })
