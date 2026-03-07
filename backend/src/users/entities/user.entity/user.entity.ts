b246ff4dfeed52f835f0339a0fe2dbccbd67e7cd
samaelMTA <miguelcolunche0@gmail.com>
2026-03-07 02:43:35 +0100

    feat: protection on passwords

--- backend/src/users/entities/user.entity/user.entity.ts
+++ backend/src/users/entities/user.entity/user.entity.ts
@@ -10,7 +10,7 @@ export class User {
     @Column({ unique: true })
     email: string;
 
-    @Column()
+    @Column({ select: false })
     password: string;
 
     @Column({
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -12,7 +12,11 @@ export class UsersService {
     ) { }
 
     async findByEmail(email: string): Promise<User | null> {
-        return this.usersRepository.findOne({ where: { email } });
+        return this.usersRepository
+            .createQueryBuilder('user')
+            .addSelect('user.password')
+            .where('user.email = :email', { email })
+            .getOne();
     }
 
     async findById(id: string): Promise<User> {
