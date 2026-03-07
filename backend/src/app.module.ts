fe1495a4fd08256923b72153bbbe1aabd8c7f3d5
samaelMTA <miguelcolunche0@gmail.com>
2026-03-07 01:18:43 +0100

    feat: configured app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ShipmentsModule } from './shipments/shipments.module';
 import { TrackingModule } from './tracking/tracking.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ShipmentsModule, TrackingModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'translog',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    ShipmentsModule,
+    TrackingModule,
+  ],
 })
 export class AppModule {}
